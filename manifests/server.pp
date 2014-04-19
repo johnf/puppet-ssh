522483b160336ba91a46746bc4cec9dfa7a20125
John Ferlito <johnf@inodes.org>
2014-04-19 12:51:36 +1000

    Disable root

--- manifests/server.pp
+++ manifests/server.pp
@@ -12,13 +12,11 @@ class ssh::server {
     require => Package['openssh-server'],
   }
 
-  /*
   augeas { '/etc/ssh/sshd_config_disable_root_login':
     context => '/files/etc/ssh/sshd_config',
     changes => 'set PermitRootLogin no',
     notify  => Service['openssh-server'],
     require => Package['openssh-server'],
   }
-  */
 
 }
